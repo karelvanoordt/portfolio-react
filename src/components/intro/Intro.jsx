e6688fac73a6f2586e4b9dfe59d39596b6aee17f
Karel van Oordt <vanoordtkarel@gmail.com>
2022-05-11 21:44:13 -0500

    Add title animations

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './intro.css'
+
+export default function Intro() {
+  return (
+
+    <div className='i'>
+        <div className='i-left'>
+            <div className='i-left-wrapper'>
+                <h1 className='i-introduction'>Hey! My name is</h1>
+                <h2 className='i-name'>Karel</h2>
+                <div className='i-title'>
+                    <div className='i-title-wrapper'>
+                        <div className='i-title-item'>Full Stack Developer 🧑🏻‍💻</div>
+                        <div className='i-title-item'>Ruby Enthusiast 💎</div>
+                        <div className='i-title-item'>Pro cyclist wannabe 🚴🏻‍♂️</div>
+                        <div className='i-title-item'>Formula 1 die-hard fan 🏎</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div className='i-right'>
+            <div className='i-right-wrapper'>
+
+            </div>
+        </div>
+    </div>
+
+)
+}
