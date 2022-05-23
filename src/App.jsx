747070ab8ad4c325a931c15da0bd6da25b2a3d67
Karel van Oordt <vanoordtkarel@gmail.com>
2022-05-23 16:02:52 -0500

    Update toggle darkmode function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import { useState, useContext } from 'react';
 import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import ProductList from "./components/productList/ProductList";
 import Contact from "./components/contact/Contact";
 import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from './context';
 
 
 const App = () => {
+
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  
   return (
-  <div>    
+  <div style={{backgroundColor: darkMode ? "#222" : "white", color: darkMode && "white" }}>    
     
      <Toggle />
      <Intro />
      <About />
-     <ProductList />
+     <ProductList  />
      <Contact />
 
   </div>
--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './toggle.css';
 import Sun from '../../img/sun.png';
 import Moon from '../../img/moon.png';
+import { ThemeContext } from '../../context';
+import { useContext } from 'react';
 
 export default function Toggle() {
+
+  const theme = useContext(ThemeContext);
+
+  const handleClick = () => {
+    theme.dispatch({ type: "TOGGLE" })
+  }
+
+  
+
   return (
     <div className='t'>
 
         <img src={Sun} alt="" className="t-icon" />       
         <img src={Moon} alt="" className="t-icon" />  
 
-        <div 
-        className="t-button"></div>
+        <div className="t-button" onClick={handleClick}></div>
 
     </div>
   )
